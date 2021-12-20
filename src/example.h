78563751b75bdb88a0e4e1f6c97d730bed9daff1
Alex <surka2012@yandex.ru>
2021-12-20 14:22:44 +0700

    december 20 2

--- src/example.h
+++ src/example.h
@@ -1,64 +1,133 @@
 #include "LedControl.h"
 #include "binary.h"
-
-/*
- Контакт DIN на матрице – к цифровому контакту 12 на Arduino
- Контакт CLK – к цифровому контакту 11
- Контакт CS – к цифровому контакту 10
-*/
 LedControl lc = LedControl(12, 11, 10, 1);
-
-// задержка между разными рожицами:
-unsigned long delaytime = 500;
-
-// веселая рожица:
-byte hf[8] = {B00111100, B01000010, B10100101, B10000001, B10100101, B10011001, B01000010, B00111100};
-// «покерфейс»:
-byte nf[8] = {B00111100, B01000010, B10100101, B10000001, B10111101, B10000001, B01000010, B00111100};
-// грустная рожица:
-byte sf[8] = {B00111100, B01000010, B10100101, B10000001, B10011001, B10100101, B01000010, B00111100};
+int j;
+// длинна зменйки
+int lenght = 1;
+// скорость змейки
+int speed = 1000;
+unsigned long timer;
+// карта
+int mapp2[8][8] = {
+    {1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1}};
+// положение фрукта
+int fruit[] = {1, 2};
+// текущая позиция на карте
+int currentPosition[] = {3, 3};
+// направления движения
+int directionX = 0;
+int directionY = 0;
+// кнопки для управления
+int buttonRight = 3;
+int buttonLeft = 2;
+int buttonUp = 1;
+int buttonDown = 13;
 
 void setup()
 {
+    pinMode(buttonRight, INPUT);
+    pinMode(buttonLeft, INPUT);
+    pinMode(buttonUp, INPUT);
+    pinMode(buttonDown, INPUT);
+
+    Serial.begin(9600);
     lc.shutdown(0, false);
     // выставляем яркость на среднее значение:
     lc.setIntensity(0, 8);
     // очищаем дисплей:
     lc.clearDisplay(0);
+    Serial.println();
+}
+
+void moveLeft()
+{
+    if (directionX == -1)
+    {
+        lc.setLed(0, j, 7, 1);
+        Serial.println(j);
+        j--;
+    }
+}
+
+void moveRight()
+{
+    if (directionX == 1)
+    {
+        lc.setLed(0, 0, j, 1);
+        Serial.println(j);
+        j++;
+    }
+}
+void moveUp()
+{
+
+    if (directionY == 1)
+    {
+        lc.setLed(0, j, 1, 1);
+        Serial.println(j);
+        j++;
+    }
+}
+void moveDown()
+{
+
+    if (directionY == -1)
+    {
+        lc.setLed(0, 7, j, 1);
+        Serial.println(j);
+        j--;
+    }
 }
 
 void loop()
 {
-    // показываем грустную рожицу:
-    lc.setRow(0, 0, sf[0]);
-    lc.setRow(0, 1, sf[1]);
-    lc.setRow(0, 2, sf[2]);
-    lc.setRow(0, 3, sf[3]);
-    lc.setRow(0, 4, sf[4]);
-    lc.setRow(0, 5, sf[5]);
-    lc.setRow(0, 6, sf[6]);
-    lc.setRow(0, 7, sf[7]);
-    delay(delaytime);
+    if (digitalRead(buttonUp))
+    {
+        j = 0;
+        directionY = 1; // двигаемся вверх
+        directionX = 0;
+    }
+    else if (digitalRead(buttonDown))
+    {
+        j = 8;
+        directionY = -1; // двигаемся вниз
+        directionX = 0;
+    }
+    else if (digitalRead(buttonRight))
+    {
+        j = 0;
 
-    // показываем «покерфейс»:
-    lc.setRow(0, 0, nf[0]);
-    lc.setRow(0, 1, nf[1]);
-    lc.setRow(0, 2, nf[2]);
-    lc.setRow(0, 3, nf[3]);
-    lc.setRow(0, 4, nf[4]);
-    lc.setRow(0, 5, nf[5]);
-    lc.setRow(0, 6, nf[6]);
-    lc.setRow(0, 7, nf[7]);
-    delay(delaytime);
-
-    // показываем веселую рожицу:
-    lc.setRow(0, 0, hf[0]);
-    lc.setRow(0, 1, hf[1]);
-    lc.setRow(0, 2, hf[2]);
-    lc.setRow(0, 3, hf[3]);
-    lc.setRow(0, 4, hf[4]);
-    lc.setRow(0, 5, hf[5]);
-    lc.setRow(0, 6, hf[6]);
-    lc.setRow(0, 7, hf[7]);
-    delay(delaytime);
+        directionY = 0; // двигаемся вправо
+        directionX = 1;
+    }
+    else if (digitalRead(buttonLeft))
+    {
+        j = 8;
+        directionY = 0; // двигаемся влево
+        directionX = -1;
+    }
+    Serial.print("digitalRead(buttonUp)  ");
+    Serial.println(digitalRead(buttonUp));
+    Serial.print("digitalRead(buttonDown)  ");
+    Serial.println(digitalRead(buttonDown));
+    Serial.print("digitalRead(buttonRight)  ");
+    Serial.println(digitalRead(buttonRight));
+    Serial.print("digitalRead(buttonLeft)  ");
+    Serial.println(digitalRead(buttonLeft));
+    // раз в заданное время делаем шаг
+    if (millis() - timer > speed)
+    {
+        timer = millis();
+        moveLeft();
+        moveUp();
+        moveDown();
+        moveRight();
+    }
 }
